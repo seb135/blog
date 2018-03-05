29578f2f0edc1fee24e5e849a1499573739a553c
seb135 <sebastiaan_honders@hotmail.com>
2018-03-05 16:43:07 +0100

    added files

---
+++ src/assets/components/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HeaderComponent extends React.Component{
+  render(){
+    return(
+      <header>
+      HeaderComponent
+      </header>
+    );
+  }
+}
+
+export default HeaderComponent;
