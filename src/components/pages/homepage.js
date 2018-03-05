c96393c1a1b55b44cde3f5968624b95f71064bbf
seb135 <sebastiaan_honders@hotmail.com>
2018-03-05 16:48:43 +0100

    added

---
+++ src/components/pages/homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HomepageComponent extends React.Component{
+  render(){
+    return(
+      <div class='main'>
+        HomepageComponent
+      </div>
+    );
+  }
+}
+
+export default HomepageComponent;
